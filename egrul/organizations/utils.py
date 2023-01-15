7e4fff6fde64fb25275d24bbcb9837ee2a0cabac
rofel <vgolosyandriya@yandex.ru>
2023-01-15 21:49:02 +0300

    FIAS address sctructure support added (from EGRUL to EGRUL_406)

--- egrul/organizations/utils.py
+++ egrul/organizations/utils.py
@@ -1,28 +1,73 @@
 import xml.etree.ElementTree
-from typing import List
+from typing import List, Literal
 
 from .models import Organization
+
+
+def cast_reg_pochta_to_constitute(region_name: str) -> str:
+    """Преобразует название региона
+     в соответствии со ст. 65 Конституцией РФ."""
+    region = region_name.upper()
+    if 'КЕМЕРОВСКАЯ' in region:
+        return 'КЕМЕРОВСКАЯ ОБЛАСТЬ - КУЗБАСС'
+    elif 'ЧУВАШИЯ' in region:
+        return 'ЧУВАШСКАЯ РЕСПУБЛИКА - ЧУВАШИЯ'
+    elif 'ХАНТЫ' in region and 'МАНСИ' in region:
+        return 'ХАНТЫ-МАНСИЙСКИЙ АВТОНОМНЫЙ ОКРУГ - ЮГРА'
+    elif 'ЯМАЛО' in region and 'НЕНЕЦКИЙ' in region:
+        return 'ЯМАЛО-НЕНЕЦКИЙ АВТОНОМНЫЙ ОКРУГ'
+    elif 'МОСКВА' in region:
+        return 'Г. МОСКВА'
+    elif 'САНКТ' in region and 'ПЕТЕРБУРГ' in region:
+        return 'Г. САНКТ-ПЕТЕРБУРГ'
+    elif 'СЕВАСТОПОЛЬ' in region:
+        return 'Г. СЕВАСТОПОЛЬ'
+    if 'РЕСПУБЛИКА' in region:
+        if region.split()[0].endswith('КАЯ'):
+            return region
+        else:
+            return f'РЕСПУБЛИКА {region.split("РЕСПУБЛИКА")[0].rstrip()}'
+
+    return region
 
 
 def get_geography_string(
         data_info: xml.etree.ElementTree.Element,
         _type: str,
-        name: str) -> str:
+        name: str,
+        addr_format: Literal['KLADR', 'FIAS'] = 'KLADR') -> str:
     """Выбирает расположение ключевых слов адреса
     в строке по типу:
-    г. Тестовый - для городов федерального значения,
-    {Тип региона} название региона - для всего остального
+    Г. МОСКВА - для городов федерального значения,
+    {Республика} Татарстан - для всего остального
     пустая строка - если поле не заполнено
     """
 
     if data_info is not None:
         data_info = data_info.attrib
+
         data_type = data_info.get(_type, '')
         data_name = data_info.get(name, '')
-        if data_name.lower() == 'город':
-            return f'{data_name} {data_type}, '
+
+        # В структуре ФИАС иногда встречаются лишние точки.
+        if addr_format == 'FIAS':
+            data_type = data_type.replace('.', '')
+            data_type = data_type + '.'
+
+        if data_name == 'ГОРОД':
+            return f'Г. {data_type}, '
         return f'{data_type} {data_name}, '
     return ''
+
+
+def line_formatter(strq: str, length: int) -> str:
+    """Убирает лишний знак прочерка из адресной строки."""
+
+    if len(strq) <= length:
+        strq = strq.replace('-', '')
+    if strq:
+        strq += ', '
+    return strq
 
 
 def get_address(
@@ -35,37 +80,69 @@ def get_address(
     index = main_address_info_attrib.get('Индекс', '000000')
 
     house = main_address_info_attrib.get('Дом', '')
-    house = house.replace('-', '')
-    if house:
-        house += ', '
+    house = line_formatter(house, 2)
     building = main_address_info_attrib.get('Корпус', '')
-    building = building.replace('-', '')
-    if building:
-        building += ', '
+    building = line_formatter(building, 2)
     flat = main_address_info_attrib.get('Кварт', '')
-    flat = flat.replace('-', '')
-    if flat:
-        flat += ', '
+    flat = line_formatter(flat, 2)
 
-    street_info = main_address_info.find('Улица')
-    street = get_geography_string(street_info,
+    street = get_geography_string(main_address_info.find('Улица'),
                                   'ТипУлица',
                                   'НаимУлица')
 
-    region_info = main_address_info.find('Регион')
-    region = get_geography_string(region_info,
+    region = get_geography_string(main_address_info.find('Регион'),
                                   'НаимРегион',
                                   'ТипРегион')
-    city_info = main_address_info.find('Город')
-    city = get_geography_string(city_info,
+    region = region.replace(',', '').strip()
+    region = cast_reg_pochta_to_constitute(region)
+    region += ', '
+    city = get_geography_string(main_address_info.find('Город'),
                                 'ТипГород',
                                 'НаимГород')
-    locality_info = main_address_info.find('НаселПункт')
-    locality = get_geography_string(locality_info,
+
+    locality = get_geography_string(main_address_info.find('НаселПункт'),
                                     'ТипНаселПункт',
                                     'НаимНаселПункт')
 
     return f'{street}{house}{building}{flat} {region}{city}{locality}{index}'
+
+
+def get_fias_address(
+        main_address_info: xml.etree.ElementTree.Element
+) -> str:
+    """Возвращает фактический адрес организации
+     в виде строки из формата КЛАДР."""
+    index = main_address_info.get('Индекс', '000000')
+
+    region_name = main_address_info.find('НаимРегион').text
+
+    if region_name:
+        region_name = region_name.upper() + ', '
+        if region_name.startswith('Г.'):
+            region_name = region_name.replace('Г.', 'Г. ')
+
+    city = get_geography_string(main_address_info.find('НаселенПункт'),
+                                _type='Вид',
+                                name='Наим',
+                                addr_format='FIAS')
+
+    street = get_geography_string(main_address_info.find('ЭлУлДорСети'),
+                                  _type='Тип',
+                                  name='Наим',
+                                  addr_format='FIAS')
+
+    house = get_geography_string(main_address_info.find('Здание'),
+                                 _type='Тип',
+                                 name='Номер',
+                                 addr_format='FIAS')
+
+    flat = get_geography_string(main_address_info.find('ПомещЗдания'),
+                                _type='Тип',
+                                name='Номер',
+                                addr_format='FIAS')
+
+    factual_address = f'{street}{house}{flat}{region_name}{city}{index}'
+    return factual_address
 
 
 def get_organization_objects(
@@ -78,13 +155,16 @@ def get_organization_objects(
     main_info = element.attrib
     name_info = element.find('СвНаимЮЛ').attrib
     full_name = name_info['НаимЮЛПолн'].strip()
-
-    main_address_info = element.find('СвАдресЮЛ/АдресРФ')
-
-    main_address_info_attrib = main_address_info.attrib
-    region_code = main_address_info_attrib.get('КодРегион', '00')
-    factual_address = get_address(main_address_info)
-
+    main_address_info = element.find('СвАдресЮЛ/СвАдрЮЛФИАС')
+    if main_address_info:
+        region_code = main_address_info.find('Регион').text
+        factual_address = get_fias_address(main_address_info)
+    else:
+        main_address_info = element.find('СвАдресЮЛ/АдресРФ')
+        main_address_info_attrib = main_address_info.attrib
+        region_code = main_address_info_attrib.get('КодРегион', '00')
+        factual_address = get_address(main_address_info)
+        factual_address = " ".join(factual_address.split())
     if name_info.get('НаимЮЛСокр') and len(name_info.get('НаимЮЛСокр')) < 4:
         short_name = None
     else:
@@ -106,14 +186,20 @@ def get_organization_objects(
             else:
                 full_branch_name = f'{full_name}. ФИЛИАЛ'
 
-            branch_main_address = branch.find('АдрМНРФ')
+            branch_address_info = branch.find('АдрМНФИАС')
 
-            if branch_main_address:
-                branch_region_code = branch_main_address.attrib.get('КодРегион', '00')
-                branch_main_address = get_address(branch_main_address)
+            if branch_address_info:
+                branch_region_code = branch_address_info.find('Регион').text
+                branch_main_address = get_fias_address(branch_address_info)
             else:
-                branch_main_address = 'НЕ УКАЗАН'
-                branch_region_code = '00'
+                branch_main_address = branch.find('АдрМНРФ')
+
+                if branch_main_address:
+                    branch_region_code = branch_main_address.attrib.get('КодРегион', '00')
+                    branch_main_address = get_address(branch_main_address)
+                else:
+                    branch_main_address = 'НЕ УКАЗАН'
+                    branch_region_code = '00'
 
             branch_kpp = branch.find('СвУчетНОФилиал')
 
