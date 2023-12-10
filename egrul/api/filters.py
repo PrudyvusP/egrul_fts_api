9c8f2958e64e17345a2b7b9ce59a387ba3d1d3e5
rofel <vgolosyandriya@yandex.ru>
2023-12-10 12:01:10 +0300

    profiling /api/organizations/?search to filters (#10)

--- egrul/api/filters.py
+++ egrul/api/filters.py
@@ -1,6 +1,25 @@
-from rest_framework import filters
+import django_filters
+
+from organizations.models import Organization
 
 
-class SearchFilterWithCustomDescription(filters.SearchFilter):
-    search_title = 'Поиск'
-    search_description = 'Параметр поиска по ИНН, КПП, ОГРН'
+class OrganizationFilter(django_filters.FilterSet):
+    """Фильтрация по реквизитам организации."""
+
+    inn = django_filters.CharFilter(label='Фильтрация по ИНН организации',
+                                    lookup_expr='exact',
+                                    min_length=10,
+                                    max_length=12)
+    kpp = django_filters.CharFilter(label='Фильтрация по КПП организации',
+                                    lookup_expr='exact',
+                                    min_length=9,
+                                    max_length=9
+                                    )
+    ogrn = django_filters.CharFilter(label='Фильтрация по ОГРН организации',
+                                     lookup_expr='exact',
+                                     min_length=13,
+                                     max_length=13)
+
+    class Meta:
+        model = Organization
+        fields = ('inn', 'kpp', 'ogrn')
